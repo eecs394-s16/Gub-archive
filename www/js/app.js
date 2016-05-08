72f825b0d467ac29af66843ca72a384c03852684
hayleyhu <yuehu2018@u.northwestern.edu>
2016-05-08 13:16:44 -0500

    back to tab/dash

--- www/js/app.js
+++ www/js/app.js
@@ -83,7 +83,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     templateUrl: 'templates/map.html',
     controller: 'MapCtrl'
   });
-$urlRouterProvider.otherwise('/map');
+$urlRouterProvider.otherwise('/tab/dash');
   // if none of the above states are matched, use this as the fallback
 
 });
